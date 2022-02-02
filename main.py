156e9413e12a778fa3b65b3f2eb4a77287c68819
Behnam Khanbeigi <b.khanbeigi@gresb.com>
2022-02-02 22:33:26 +0100

    Improve change title mechanism be usable for other events

--- main.py
+++ main.py
@@ -2,33 +2,42 @@ import os
 from datetime import date
 import re
 import telebot
+from dotenv import load_dotenv
+from persiantools import digits
 
+
+load_dotenv()
+
+EVENT = os.environ['EVENT']
+DATE = os.environ['DATE'] # Iso format 2022-04-19
+CONCAT_STR = os.environ['CONCAT_STR']
 API_KEY = os.environ['API_KEY']
+
 bot = telebot.TeleBot(API_KEY)
 
 def daysCounter():
   today = date.today()
-  nowRooz = date(2022, 3, 21)
-  delta = nowRooz - today
+  date_of_event = date.fromisoformat(DATE)
+  delta = date_of_event - today
   return delta.days
 
-def addDaysToTitle(title, daysToNowRooz):
-  selectedNumber = re.findall(r'\[(\d+)\]', title)
-  newTitle = ""
-  if len(selectedNumber) > 0 :
-    if selectedNumber[0] == daysToNowRooz:
-      # skip for the same day
-      return title
-    newTitle = title.replace(selectedNumber[0], daysToNowRooz)
+def addDaysToTitle(title, daysToEvent):
+  strDaysToEvent = digits.en_to_fa(str(daysToEvent))
+  selectedTitlePart = re.findall(r'\«(\d+\s.*?)\»', title)
+  newTitlePart = strDaysToEvent + CONCAT_STR + EVENT
+  if len(selectedTitlePart) > 0 :
+    newTitle = title.replace(selectedTitlePart[0], newTitlePart)
   else:
-    newTitle = title + " ["+daysToNowRooz+"]"
+    # FIX: RTL issue.
+    # Maybe adding a farsi character in the begiging solve the issue!
+    newTitle = title + "«" + newTitlePart + "»"
   return newTitle
 
 def titleGenerator(prevTitle):
-  daysToNowRooz = daysCounter()
-  if daysToNowRooz < 1:
+  daysToEvent = daysCounter()
+  if daysToEvent < 1:
     return
-  newTitle = addDaysToTitle(prevTitle , str(daysToNowRooz))
+  newTitle = addDaysToTitle(prevTitle , daysToEvent)
   print(newTitle)
   return newTitle
 
@@ -40,17 +49,17 @@ def greet(message):
 @bot.message_handler(commands=['setTitle'])
 def setTitle(message):
   prevTitle = message.chat.title
-  nowRoozTitle = titleGenerator(prevTitle)
+  eventTitle = titleGenerator(prevTitle)
   print(message.chat.id)
-  bot.set_chat_title(message.chat.id,nowRoozTitle)
+  bot.set_chat_title(message.chat.id,eventTitle)
 
 #temprory solution to lissten others message
 @bot.message_handler()
 def checkMessages(message):
   prevTitle = message.chat.title
-  nowRoozTitle = titleGenerator(prevTitle)
+  eventTitle = titleGenerator(prevTitle)
   print(message.chat.id)
-  bot.set_chat_title(message.chat.id,nowRoozTitle)
+  bot.set_chat_title(message.chat.id,eventTitle)
 
 # chat_id = -651564694
 # bot.send_message(chat_id, "sag")
